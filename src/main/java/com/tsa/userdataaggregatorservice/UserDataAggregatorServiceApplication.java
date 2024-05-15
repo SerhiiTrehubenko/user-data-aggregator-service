b5d7bd1c54fcb8d4929834eac6d20cad98d890b3
Serhii Trehubenko <tsa85.ca@gmail.com>
2024-05-15 09:59:41 +0300

    0-init

---
+++ src/main/java/com/tsa/userdataaggregatorservice/UserDataAggregatorServiceApplication.java
@@ -0,0 +1,13 @@
+package com.tsa.userdataaggregatorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDataAggregatorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserDataAggregatorServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/tsa/userdataaggregatorservice/UserDataAggregatorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tsa.userdataaggregatorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDataAggregatorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
